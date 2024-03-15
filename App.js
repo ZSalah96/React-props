258bebc509b2422b2f9141a0ab9d6c665322ecb7
ZSalah96 <154322724+ZSalah96@users.noreply.github.com>
2024-03-15 22:12:36 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  const name = 'Salah';
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Welcome to my React App.</p>
+    </div>
+  );
+};
+
+export default App;
